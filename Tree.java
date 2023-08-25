40b6a5f41bfcac580e64be503723f53536e77bc1
Zahari Romanov <140546948+zahromdev@users.noreply.github.com>
2023-08-25 08:43:31 -0400

    setup

---
+++ Tree.java
@@ -0,0 +1,17 @@
+public class Tree {
+    public static void main(String[] args){
+    /*
+    Plant a Tree.java , 
+    save the Coffee bean from exctinction!
+    */    
+        System.out.println("I am but a humble coffee tree, only you can prevent my extinction!");
+        System.out.println("    ###    ");
+        System.out.println("   #####   ");
+        System.out.println("  #######  ");
+        System.out.println("   #####   ");
+        System.out.println("    ###    ");
+        System.out.println("     #     ");
+        System.out.println("     |     ");
+        System.out.println("_____|_____");
+    }
+}
---
+++ helloworld.java
@@ -0,0 +1,27 @@
+/* 
+How - To
+Multi-Line
+Comment
+ */
+
+/** 
+ * This is a documentation comment to create documentation for APIs.
+ * They are available to the end user to see and read. 
+ */
+
+ // datatype variableNamve
+ // int age;
+ // double salaryRequirement;
+ // boolean isEmployed;
+// Prints 'Hello World'
+
+public class helloworld {
+    public static void main(String[] args) {
+      // Prints string 
+      System.out.println("Hello World");
+      // Prints variable
+      String greeting = "Hello World";
+      System.out.println(greeting);
+  
+    }
+  }
